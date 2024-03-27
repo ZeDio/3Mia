2816a4ab933b0b56840a1ee42989f6873bce0d5d
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-27 09:31:47 -0300

    oi

---
+++ INW JS/react/site-react-vita/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Banner from './Componentes/Banner/Banner'
+import InfoPrincipal from './Componentes/ConteudoPrincipal/InfoPrincipal'
+
+function App() {
+
+  return (
+    <>
+      <Banner></Banner>
+      <InfoPrincipal/>
+    </>
+  )
+}
+
+export default App
