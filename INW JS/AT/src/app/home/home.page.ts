0a5fa4114318d8b3e2b581c5472d942cff39d89c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-17 12:13:42 -0300

    melhor que funcione dessa vez

---
+++ INW JS/AT/src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor() {}
+  pergunta1 = ''
+  pergunta2 = ''
+  pergunta3 = ''
+  pergunta4 = ''
+  pergunta5 = ''
+  pergunta6 = ''
+  pergunta7 = ''
+  pergunta8 = ''
+  pergunta9 = ''
+  pergunta10 = ''
+
+  exibirmensagem(){
+
+  }
+
+}
