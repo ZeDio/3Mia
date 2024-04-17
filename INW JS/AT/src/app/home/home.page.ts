b3119268e2f085335ed26e97b620b62fdb889720
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-17 13:16:10 -0300

    modificações at

--- INW JS/AT/src/app/home/home.page.ts
+++ INW JS/AT/src/app/home/home.page.ts
@@ -17,9 +17,23 @@ export class HomePage {
   pergunta8 = ''
   pergunta9 = ''
   pergunta10 = ''
+  pergunta11 = ''
+  soma = 0
+  resp = ''
 
-  exibirmensagem(){
+  acertaranimal(){
+    this.soma = parseInt(this.pergunta1)+parseInt(this.pergunta2)+parseInt(this.pergunta3)+parseInt(this.pergunta4)+parseInt(this.pergunta5)+parseInt(this.pergunta6)+parseInt(this.pergunta7)+parseInt(this.pergunta8)+parseInt(this.pergunta9)+parseInt(this.pergunta10)+parseInt(this.pergunta11)
 
+    if(this.soma == 2100){
+      this.resp = "leão"
+    }
+    if(this.soma == 1550){
+      this.resp = "baleia"
+    }
+    else{
+      this.resp = "não existe"
+    }
   }
+
 
 }
