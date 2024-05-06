4fbe25c9626557819fe2e3b08add332e33bd2502
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 11:34:12 -0300

    começando

---
+++ INW JS/CineApp/BlackCine/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Componentes/Header"
+import Navbar from "./Componentes/Navbar"
+
+function App() {
+  
+  return (
+    <>
+      <Header>
+        <Navbar></Navbar>
+      </Header>
+    </>
+  )
+}
+
+export default App
---
+++ INW JS/CineApp/BlackCine/src/Pages/PageNoteFoud.jsx
@@ -0,0 +1,7 @@
+function PageNoteFound() {
+    return ( 
+        <h1>Erro 404</h1>
+     );
+}
+
+export default PageNoteFound;
