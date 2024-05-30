57179d6f8e5edcf49bb384b56b56a03157d2f1be
ZeDio <jose.diogo100407@gmail.com>
2024-05-30 12:10:51 -0300

    ac

--- INW JS/CineApp/BlackCine/src/Componentes/Header/Header.jsx
+++ INW JS/CineApp/BlackCine/src/Componentes/Header/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from "../Navbar/Navbar";
 
 function Header() {
     return ( 
-        <header className="flex bg-primary-color text-white p-4 justify-between">
+        <header className="flex w-full fixed top-3 bg-primary-color text-white p-4 justify-between">
             <span>Olá, visitante!</span>
             <h1>Bem-vindo ao BlackCine</h1>
             <Navbar/>
--- INW JS/CineApp/BlackCine/src/Pages/Home.jsx
+++ INW JS/CineApp/BlackCine/src/Pages/Home.jsx
@@ -4,19 +4,19 @@ function Home() {
     console.log(data)
     return ( 
         <>
-        <div className='grid grid-cols-3 gap-20'>
+        <div className='grid grid-cols-3 gap-20 m-10 p-10'>
             {
                 data.map(
                     (filme, index) => (
                         <>
                         <div className='card' key={index}>
-                            <h1 key={filme.title}>{filme.title}</h1>
-                            <img src={filme.image}/>
+                            <h1 className='text-center' key={filme.title}>{filme.title}</h1>
+                            <img className='rounded-2xl w-screen h-52' src={filme.image}/>
 
                             <div className='tags'>
                                 {
                                 filme.tags.map(tag =>(
-                                    <span className='bg-pink-800 text-white p-1 m-1' key={tag}>{tag}</span>
+                                    <span className='bg-black text-white p-1 m-1' key={tag}>{tag}</span>
                                 )) 
                                 }
                             </div>
