82c8532d6a0ab6e0c894fd81a53be77f415d64ee
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-10 11:28:24 -0300

    sla

--- Games/Flappy Bird/Assets/scripts/obstaculo.cs
+++ Games/Flappy Bird/Assets/scripts/obstaculo.cs
@@ -8,13 +8,17 @@ public class obstaculo : MonoBehaviour
     private float velocidade = 0.8f;
     [SerializeField]
     private float variaçãoPosiçãoY;
+    private Vector3 posicaoPassaro;
+    private bool pontuei;
+    private UIcontroler controladorUI;
+
     // Update is called once per frame
 
     private void Start()
     {
-        
+        this.posicaoPassaro = GameObject.FindAnyObjectByType<passaro>().transform.position;
+        this.controladorUI = GameObject.FindAnyObjectByType<UIcontroler>();
     }
-
     private void Awake()
     {
         this.transform.Translate(Vector3.up * Random.Range(-variaçãoPosiçãoY, variaçãoPosiçãoY));        
@@ -22,7 +26,11 @@ public class obstaculo : MonoBehaviour
     void Update()
     {
         this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
-
+        if(!this.pontuei && this.transform.position.x < this.posicaoPassaro.x)
+        {
+            this.controladorUI.adicionarPontos();
+            this.pontuei = true;
+        }       
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
