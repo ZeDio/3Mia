e45fe6d96c160decc19726bddb8e7a375ebe1445
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-07 08:47:22 -0300

    react

---
+++ INW JS/recomecoReact/recomeco/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  
+  const [contador, setContador] = useState(0);
+  function handleClick(){
+    setContador((contador)=>{
+      contador + 1
+    })
+    console.log(contador)
+  }
+
+  return (
+    <>
+     <p>{contador}</p>
+     <button onClick={handleClick}>Aumentar</button>
+    </>
+  )
+}
+
+export default App
